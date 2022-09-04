92072db38c617a1003afb6bdfd72cb1f0dffd9cc
Shivanshu <110782261+ershiva23@users.noreply.github.com>
2022-09-04 15:22:49 +0530

    Create Majority element.cpp

---
+++ Arrays/Majority element.cpp
@@ -0,0 +1,31 @@
+ int majorityElement(int a[], int size)
+    {
+        
+        // your code here
+         int count=1; int res=0;
+    for(int i=1; i<size; i++){
+        if(a[res] == a[i])
+            count++;
+        else
+            count--;
+            
+        if(count==0){
+            count=1;
+            res=i;
+        }
+    }
+    
+    //2nd   res 
+    count=0;
+    for(int i=0;i<size;i++){
+        if(a[res]==a[i])
+            count++;
+    }
+    
+    if(count <= (size/2))
+        return -1;
+    else
+        return a[res];
+}
+        
+    
